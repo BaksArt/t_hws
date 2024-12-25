88cdceb2fe6695267bf2e5e5f67630a2ef29d6f9
BaksArt <arshchelkov@gmail.com>
2024-12-25 20:31:30 +0300

    Initial commit

---
+++ src/main/java/ru/tbank/hse/fd/streamPractise/Main.java
@@ -0,0 +1,14 @@
+package main.java.ru.tbank.hse.fd.streamPractise;
+
+import main.java.ru.tbank.hse.fd.streamPractise.model.Car;
+import main.java.ru.tbank.hse.fd.streamPractise.model.Owner;
+import main.java.ru.tbank.hse.fd.streamPractise.service.CarService;
+import main.java.ru.tbank.hse.fd.streamPractise.utils.Condition;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
